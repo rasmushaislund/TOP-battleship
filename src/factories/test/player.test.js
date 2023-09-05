11028088a364cc8ecf2ffb8e60d9a48bb2d0d949
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-09-05 21:51:07 +0200

    Add tests for player names and activePlayer

--- src/factories/test/player.test.js
+++ src/factories/test/player.test.js
@@ -1,3 +1,31 @@
 // START //
 
 import { Player } from '../player';
+import { Gameboard } from '../gameboard';
+import { Ship } from '../ship';
+
+describe('Testing Player class interfaces', () => {
+  let player = Player;
+  let gameboard = Gameboard;
+  let ship = Ship;
+
+  beforeEach(() => {
+    player = new Player('Rasmus');
+    gameboard = new Gameboard();
+    gameboard.buildBoard();
+    ship = new Ship('Patrol Boat', 2, true);
+  });
+
+  test('Is name of playerOne Rasmus?', () => {
+    expect(player.playerOneName).toEqual('Rasmus');
+  });
+
+  test('PlayerTwo name is computer', () => {
+    expect(player.playerTwoName).toEqual('computer');
+  });
+
+  test('activePlayer can be changed to playerTwo', () => {
+    player.activePlayer = player.playerTwoName;
+    expect(player.activePlayer).toEqual('computer');
+  });
+});
