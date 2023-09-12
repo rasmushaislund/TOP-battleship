7c739d2b88586ec724faba3d46ef9978765ac529
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-09-12 13:28:21 +0200

    Add Gameboard.buildBoard() to attack functions
    In order to make tests pass.

--- src/factories/player.js
+++ src/factories/player.js
@@ -6,10 +6,43 @@ export class Player {
   playerOneName;
   playerTwoName = 'computer';
   activePlayer = this.playerOneName;
+  attacks;
 
-  constructor(playerOneName) {
-    this.playerOneName = playerOneName;
+  constructor(playerName) {
+    this.playerName = playerName;
+    this.attacks = [];
   }
 
-  attackSquare(row, column) {}
+  attackSquare(row, column) {
+    const gameboard = new Gameboard();
+    // gameboard.buildBoard();
+
+    if (!this.hasBeenAttacked(row, column)) {
+      this.attacks.push([row, column]);
+      gameboard.receiveAttack(row, column);
+    } else {
+      return;
+    }
+  }
+
+  attackRandomSquare() {
+    const gameboard = new Gameboard();
+    // gameboard.buildBoard();
+
+    if (this.attacks.length >= 100) return;
+    const randRow = Math.floor(Math.random() * gameboard.gridSize);
+    const randColumn = Math.floor(Math.random() * gameboard.gridSize);
+
+    if (!this.hasBeenAttacked(randRow, randColumn))
+      gameboard.receiveAttack(randRow, randColumn);
+    return;
+  }
+
+  hasBeenAttacked(row, column) {
+    for (let i = 0; i < this.attacks.length; i++) {
+      if (this.attacks[i][0] === row && this.attacks[i][1] === column)
+        return true;
+    }
+    return false;
+  }
 }
