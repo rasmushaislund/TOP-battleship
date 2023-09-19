6e5b3f6f3db17ad4391fc9bc571d383b99bc5c7d
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-09-19 13:46:08 +0200

    Add function to show modal on page load

--- src/index.js
+++ src/index.js
@@ -4,3 +4,17 @@ import './style.css';
 import { Ship } from './factories/ship';
 import { Gameboard } from './factories/gameboard';
 import { Player } from './factories/player';
+
+// Set copyright year automatically
+const copyrightSpan = document.querySelector('#copyright-span');
+copyrightSpan.textContent = new Date().getFullYear();
+
+// Show modal with page load
+
+// window.addEventListener('load', () => {
+//   const modal = document.querySelector('.modal');
+//   modal.showModal();
+//   modal.classList.add('show');
+// });
+
+// END //
