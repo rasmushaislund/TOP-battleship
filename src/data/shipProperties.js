7ee1fecbf4da712752406a0b6f14f0caf8a0a91b
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-02 15:08:05 +0200

    Add color key

--- src/data/shipProperties.js
+++ src/data/shipProperties.js
@@ -4,21 +4,26 @@ export const shipProperties = [
   {
     type: 'Carrier',
     length: 5,
+    color: 'rgb(252, 4, 4, .9)',
   },
   {
     type: 'Battleship',
     length: 4,
+    color: 'rgb(4, 140, 4, .9)',
   },
   {
     type: 'Destroyer',
     length: 3,
+    color: 'rgb(4, 4, 252, .9)',
   },
   {
     type: 'Submarine',
     length: 3,
+    color: 'rgb(252, 251, 32, .9)',
   },
   {
     type: 'Patrol Boat',
     length: 2,
+    color: 'rgb(12, 4, 12)',
   },
 ];
