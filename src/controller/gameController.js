a0ce50582c17ed8e6c40f5b63db8bc4130406188
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-02 15:00:00 +0200

    Add code for initializing Gameboards and 2D arrays

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -2,5 +2,22 @@
 
 import { Gameboard } from '../factories/gameboard';
 import { Player } from '../factories/player';
+import { Display } from './displayController';
+
+export function Game() {
+  const playerBoard = new Gameboard();
+  const aiBoard = new Gameboard();
+
+  const buildPlayerBoard = playerBoard.buildBoard();
+  const buildAiBoard = aiBoard.buildBoard();
+
+  console.log(playerBoard, aiBoard);
+
+  return {
+    gridSize: playerBoard.gridSize,
+    player2dArray: playerBoard.board,
+    ai2dArray: aiBoard.board,
+  };
+}
 
 // END //
