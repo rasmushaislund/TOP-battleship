e33d5fcb6cbd010da901a288502e4af63d277b02
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-03 14:25:01 +0200

    Adjust color-key value

--- src/data/shipProperties.js
+++ src/data/shipProperties.js
@@ -4,26 +4,26 @@ export const shipProperties = [
   {
     type: 'Carrier',
     length: 5,
-    color: 'rgb(252, 4, 4, .9)',
+    color: 'rgb(252, 4, 4, .4)',
   },
   {
     type: 'Battleship',
     length: 4,
-    color: 'rgb(4, 140, 4, .9)',
+    color: 'rgb(4, 140, 4, .4)',
   },
   {
     type: 'Destroyer',
     length: 3,
-    color: 'rgb(4, 4, 252, .9)',
+    color: 'rgb(4, 4, 252, .4)',
   },
   {
     type: 'Submarine',
     length: 3,
-    color: 'rgb(252, 251, 32, .9)',
+    color: 'rgb(252, 251, 32, .4)',
   },
   {
     type: 'Patrol Boat',
     length: 2,
-    color: 'rgb(12, 4, 12)',
+    color: 'rgb(12, 4, 12, .4)',
   },
 ];
