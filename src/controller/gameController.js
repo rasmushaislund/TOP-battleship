3cbceb4918ad2c6d1fb88b7bfa0dd3ea31a99b4e
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-04 15:46:01 +0200

    Add building blocks to switch player & play round

--- src/controller/gameController.js
+++ src/controller/gameController.js
@@ -2,9 +2,9 @@
 
 import { Gameboard } from '../factories/gameboard';
 import { Player } from '../factories/player';
-import { Display } from './displayController';
 
-export function Game() {
+export function Game(playerName, aiName) {
+  // Initialize gameboard and place ships
   const playerBoard = new Gameboard();
   const aiBoard = new Gameboard();
 
@@ -14,12 +14,65 @@ export function Game() {
   const placeShipsPlayer = playerBoard.getRandomPlacement();
   const placeShipsAi = aiBoard.getRandomPlacement();
 
-  console.log(playerBoard.board, aiBoard.board);
+  // Initialize players and handle player's turn
+  const players = [
+    {
+      name: playerName,
+    },
+    {
+      name: aiName,
+    },
+  ];
+
+  const player = new Player(players[0].name);
+  const ai = new Player(players[1].name);
+
+  let activePlayer = players[0];
+  const switchPlayerTurn = () => {
+    if (activePlayer === players[0]) {
+      activePlayer = players[1];
+    }
+    activePlayer = players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+
+  // Play a round of the game
+  let winner;
+  const playRound = (row, column) => {
+    if (getActivePlayer() === players[0]) {
+      player.attackSquare(row, column);
+    }
+    ai.attackRandomSquare();
+
+    // Check for a winner
+    if (playerBoard.allShipsSunk) {
+      winner = players[0].name;
+    } else if (aiBoard.allShipsSunk) {
+      winner = players[1].name;
+    }
+
+    switchPlayerTurn();
+  };
+
+  console.log(
+    playerBoard.board,
+    aiBoard.board,
+    getActivePlayer,
+    players,
+    player,
+    ai,
+  );
 
   return {
+    playerBoard,
+    aiBoard,
     gridSize: playerBoard.gridSize,
     player2dArray: playerBoard.board,
     ai2dArray: aiBoard.board,
+    getActivePlayer,
+    playRound,
+    winner,
   };
 }
 
