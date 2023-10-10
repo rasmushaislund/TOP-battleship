e7c23680c288bf6e5620411e181dbc3454b99f12
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-10 13:56:00 +0200

    Add push to attack array for randomAttack function

--- src/factories/player.js
+++ src/factories/player.js
@@ -11,27 +11,26 @@ export class Player {
     this.attacks = [];
   }
 
-  attackSquare(row, column) {
-    const aiBoard = Game().aiBoard;
-
+  attackSquare(row, column, enemyBoard) {
     if (!this.hasBeenAttacked(row, column)) {
       this.attacks.push([row, column]);
-      aiBoard.receiveAttack(row, column);
+      enemyBoard.receiveAttack(row, column);
     } else {
       return;
     }
   }
 
-  attackRandomSquare() {
-    const playerBoard = Game().playerBoard;
-
+  attackRandomSquare(playerBoard) {
     if (this.attacks.length >= 100) return;
     const randRow = Math.floor(Math.random() * playerBoard.gridSize);
     const randColumn = Math.floor(Math.random() * playerBoard.gridSize);
 
-    if (!this.hasBeenAttacked(randRow, randColumn))
+    if (!this.hasBeenAttacked(randRow, randColumn)) {
+      this.attacks.push([randRow, randColumn]);
       playerBoard.receiveAttack(randRow, randColumn);
-    return;
+    } else {
+      return;
+    }
   }
 
   hasBeenAttacked(row, column) {
