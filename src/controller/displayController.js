12fe9458bfe16a91c863f88138541661c026864e
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-10 13:56:41 +0200

    Add event listener for starting playRound()

--- src/controller/displayController.js
+++ src/controller/displayController.js
@@ -63,8 +63,8 @@ export function Display(playerName) {
     }
   };
 
-  // Set initial name in "waiting for player"
-  const setInitialNameWaiting = () => {
+  // Set name in "waiting for player"
+  const setNameWaiting = () => {
     const waitingForPlayer = document.querySelector('#player-id');
     const loader = document.querySelector('.loader');
     waitingForPlayer.textContent = game.getActivePlayer().name;
@@ -73,9 +73,51 @@ export function Display(playerName) {
     loader.classList.remove('invisible');
   };
 
+  // Check which player turn it is
+
   setPlayerName(player, ai);
   buildGrids();
-  setInitialNameWaiting();
+  setNameWaiting();
+
+  // Event listener for player attack on enemy
+  const opponentBoard = document.querySelector('.game-board-opponent');
+
+  opponentBoard.addEventListener('mouseup', (e) => {
+    e.preventDefault();
+    const target = e.target;
+
+    // Convert cell indexNumber to coordinates
+    const indexNumber = target.dataset.indexNumber;
+    const indexToArray = indexNumber.split('-');
+    const row = Number(indexToArray[0]);
+    const column = Number(indexToArray[1]);
+
+    // Initiate attack from player
+    game.playRound(row, column);
+
+    // Show a winner
+    if (game.winner) {
+      const winner = document.querySelector('#player-won');
+      const winnerId = document.querySelector('#winner-id');
+      const playerTurn = document.querySelector('.player-turn');
+
+      winnerId.textContent = game.winner;
+      winner.classList.remove('invisible');
+      playerTurn.classList.add('invisible');
+
+      // Disable board for further input
+      const gameBoards = document.querySelectorAll('.game-board');
+      const cells = document.querySelectorAll('.cell');
+
+      gameBoards.forEach((board) => {
+        board.classList.add('disabled-board');
+      });
+
+      cells.forEach((cell) => {
+        cell.classList.add('disabled-cell');
+      });
+    }
+  });
 }
 
 // END //
