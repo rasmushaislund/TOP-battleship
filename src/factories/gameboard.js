f5886eef04a20daf7e0cc571dcd9deccbe957a16
Rasmus H. <99361520+rasmushaislund@users.noreply.github.com>
2023-10-30 15:42:24 +0100

    Fixed condition in allShipsSunk

--- src/factories/gameboard.js
+++ src/factories/gameboard.js
@@ -131,7 +131,7 @@ export class Gameboard {
   }
 
   allShipsSunk() {
-    if (this.sunkenShips.length < 1) return false;
+    if (this.sunkenShips.length !== shipProperties.length) return false;
     return true;
   }
 
